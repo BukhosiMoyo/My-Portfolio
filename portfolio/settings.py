778ab946406eedae25b6e07b1a930e69460b6916
Bukhosi Moyo <bukhosizimcode@gmail.com>
2020-11-12 21:31:08 +0200

    changing static files sttings to server static files on the static folder

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path, os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,4 @@ urlpatterns = [
     path('superuser/login', include("login.urls")),
     path('', include("blog.urls")),
     
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
